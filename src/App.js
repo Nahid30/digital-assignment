e68263ffab4b5779f060d5fade62d5d66e873d7d
Nahid30 <nahidshuvo30@gmail.com>
2022-07-07 12:51:04 +0600

    about part done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import About from "./pages/About/About";
 import Home from "./pages/Home/Home";
 import NotFound from "./pages/NotFound/NotFound";
 import Footer from "./Shared/Footer";
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="home" element={<Home />} />
+        <Route path="about" element={<About />} />
         <Route path="*" element={<NotFound />} />
 
       </Routes>
---
+++ src/pages/About/About.js
@@ -0,0 +1,56 @@
+import React from "react";
+import about from "../../images/about.jpg";
+
+const About = () => {
+  return (
+    <div className="m-10">
+      <div className="grid grid-cols-2 gap-10">
+        <div>
+          <img className="rounded" src={about} alt="" />
+        </div>
+
+        <div>
+          <p>
+            <span className="font-semibold">Sohni Juneja</span>, a product of <span className="font-semibold">National Institute of Fashion Technology
+            (Delhi)</span> where she got her proficiency in her passion for Designing
+            and Styling. During the tenure of the course, she interned with the
+            Designer Duo, Aashima and Leena and later joined the prestigious
+            label of Designer Raghavendra Rathore as a stylist and made her
+            presence felt.
+          </p>
+          <p className="my-4">
+            You made me feel so comfortable (not to forget the soft brush touch,
+            asmr feeling), I couldn't have picked a better makeup artist.
+          </p>
+          <p className="my-4">
+            Her Quest for adding more feathers to her cap and wanting to have
+            the ability to give people a whole body makeover took her to Pearl
+            Academy, in association with Developments at London Institute (DALI)
+            at London College of Fashion, where she took up Fashion Media Makeup
+            and topped her Batch.
+          </p>
+          <p>
+            Emoting with the feminine desires of looking their best on their
+            special day, Sohni Juneja has vowed many a bride with her power to
+            transform. She understands what works on models for the runways must
+            be translated differently for the everyday woman. With her dainty,
+            yet sure strokes she can take you to ̰ completely different realms,
+            from Smokey to well-defined, she masters it all, wanting to add
+            drama to each look differently every time.
+          </p>
+        </div>
+      </div>
+
+      <p className="mt-20">Her immense innovation when it comes fashion/catwalk makeup has also made her one of the most sought after with fashion photographers like Sarang Sena known for professionalism and versatility. Sohni did 54 shoots for a famous modeling agency in 5 days in Goa. Alongside she is also working as a Freelancer for renowned international magazines like Grazia, and other modeling agencies. Her constant innovation has also gave her opportunity to showcase her excellece in Styling and Makeup at fashion shows.</p>
+      <p className="my-4">Having a keen interest in sharing her knowledge about the same Sohni Juneja also takes regular sessions in various finishing schools across the country. Sohni Juneja is one on the best Bridal makeup artist in South Delhi and providing her professional services all over india.</p>
+      <p>Sohni Juneja has been awarded The Garhwal post - Daughters of Uttarakhand award for Excellence in the art of makeup from his Excellency The Governor of Uttarakhand. She was also awarded Woman Achiever's Award 2018 organised by Amar Ujaala.</p>
+    
+    <p className="text-xl font-semibold my-10">Famous Bridal Makeup Artist In Delhi</p>
+    <p>Sohni Juneja is an extremely famous bridal makeup artist in Delhi. A professional with experience, Sohni Juneja is skilled and truly an expert in every sense of the word. Creative and incredibly passionate, she transforms every bride into nothing short of a dream. With her professionalism, Sohni Juneja can hide anything you consider a shortcoming, and assist you to exude an unmatched level of confidence.</p>
+    <p className="my-4">She is the makeup artist you need if you're in search of a glam look for your wedding day. Each bride is unique and so are her needs. To exhibit the beauty of her clients, she grasps the essence of this understanding.</p>
+
+    </div>
+  );
+};
+
+export default About;
