2b269006ecab72dc45752593373e254dca844c63
Nahid30 <nahidshuvo30@gmail.com>
2022-07-07 06:00:43 +0600

    FAQS part done

--- src/pages/Home/FAQ.js
+++ src/pages/Home/FAQ.js
@@ -1,11 +1,68 @@
-import React from 'react';
+import React from "react";
 
 const FAQ = () => {
-    return (
-        <div>
-            <h1 className='text-center font-4xl'>FAQ'S</h1>
+  return (
+    <div>
+      <h1 className="text-center text-4xl my-10">FAQ'S</h1>
+      <div className="mx-10">
+        <div
+          tabindex="0"
+          class="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box"
+        >
+          <div class="collapse-title text-xl font-medium">
+          Which locations does professional makeup artist Sohni Juneja serve?
+          </div>
+          <div class="collapse-content">
+            <p>Sohni Juneja is ranked among the Best Bridal makeup artists in Delhi, she also has a makeup studio in Dehradun and extends her makeup services worldwide.</p>
+          </div>
         </div>
-    );
+        <div
+          tabindex="0"
+          class="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box my-4"
+        >
+          <div class="collapse-title text-xl font-medium">
+          Why is Sohni Juneja considered one of the best makeup artists all over India?
+          </div>
+          <div class="collapse-content">
+            <p>Sohni Juneja is one of the best freelance makeup artists in India. She offers you all the services not only at her makeup studio but also at your respective venue. To give you a smooth and trouble-free makeover experience, she travels within the city, outstations, and at your doorstep as well.</p>
+          </div>
+        </div>
+        <div
+          tabindex="0"
+          class="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box mb-4"
+        >
+          <div class="collapse-title text-xl font-medium">
+          Why choose Sohni Juneja for Party Makeup?
+          </div>
+          <div class="collapse-content">
+            <p>The professional makeup artist in Delhi offers bridal makeup, party makeup, and hairstyling. With this Sohni Juneja offers engagement makeup, party makeup services, wedding makeup tips, and product recommendations</p>
+          </div>
+        </div>
+        <div
+          tabindex="0"
+          class="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box mb-4"
+        >
+          <div class="collapse-title text-xl font-medium">
+          What services are included in Bridal makeup packages offered by Sohni Juneja?
+          </div>
+          <div class="collapse-content">
+            <p>Sohni Juneja provides exclusive bridal makeup services to her clients. She is an expert in grooming her brides. She offers makeup services to the brides by using high-quality makeup products so that brides can get a long-lasting look.</p>
+          </div>
+        </div>
+        <div
+          tabindex="0"
+          class="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box mb-4"
+        >
+          <div class="collapse-title text-xl font-medium">
+          Does a Professional makeup artist in Delhi Sohni Juneja offer all kinds of bridal makeup services?
+          </div>
+          <div class="collapse-content">
+            <p>The professional makeup artist in Delhi offers bridal makeup, party makeup, and hairstyling. With this Sohni Juneja offers engagement makeup, party makeup services, wedding makeup tips, and product recommendations</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FAQ;
